89a9175c4dd29841afbb5bb2b0cbd5cd5dab9ec3
Owen Peterson <32946611+OwenPetersonep@users.noreply.github.com>
2024-04-10 21:26:37 -0500

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  LibraryApp
+//
+//  Created by 90308214 on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       Login()
+        }
+       
+    }
+
+
+#Preview {
+    ContentView()
+}
