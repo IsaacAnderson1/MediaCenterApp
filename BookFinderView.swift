95badd09c1b4c1f7a3d30e26cfa2d8ee40c23dbf
Isaac Anderson <95647478+IsaacAnderson1@users.noreply.github.com>
2024-05-28 10:41:07 -0500

    Update BookFinderView.swift

--- BookFinderView.swift
+++ BookFinderView.swift
@@ -1,57 +1,54 @@
 import SwiftUI
+import WebKit
 
+struct WebViewStruct: UIViewRepresentable {
+    var requestURL: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webview = WKWebView()
+        webview.navigationDelegate = context.coordinator
+        return webview
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: requestURL)
+        uiView.load(request)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebViewStruct
+        
+        init(_ parent: WebViewStruct) {
+            self.parent = parent
+        }
+        
+        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            // Handle the error
+            print(error.localizedDescription)
+        }
+        
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            // Starting to load the page
+            print("Started to load")
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            // Page loading finished
+            print("Finished loading")
+        }
+    }
+}
+
+// SwiftUI view that hosts the WebView
 struct BookFinderView: View {
-    let allBooks = ["The Great Gatsby", "1984", "To Kill a Mockingbird", "Pride and Prejudice", "War and Peace", "The Catcher in the Rye", "Moby Dick"]
-    @State private var searchText = ""
-    @Environment(\.presentationMode) var presentationMode  // Use the environment to control the presentation mode
-
-    var filteredBooks: [String] {
-        if searchText.isEmpty {
-            return allBooks
-        } else {
-            return allBooks.filter { $0.localizedCaseInsensitiveContains(searchText) }
-        }
-    }
-
     var body: some View {
-        NavigationView {
-            VStack {
-                TextField("Search for books", text: $searchText)
-                    .padding(10)
-                    .background(Color(.systemGray5))  // Light grey color for the text field background
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-                
-                List(filteredBooks, id: \.self) { book in
-                    Text(book)
-                        .padding()
-                }
-                .listStyle(PlainListStyle())
-                taskBar(selectedTab: 2)
-            }
-            .navigationBarTitle(Text("Search Books"), displayMode: .large)
-            .navigationBarItems(leading: backButton)
-            .navigationBarItems(trailing: Image("ephslogo")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 125, height: 125))
-        }
-    }
-
-    private var backButton: some View {
-        Button(action: {
-            presentationMode.wrappedValue.dismiss()
-        }) {
-            HStack {
-                Image(systemName: "arrow.left")  // System image for back arrow
-                Text("Back")
-            }
-            .foregroundColor(customRed)  // Apply the custom red color here
-        }
-    }
-
-    var customRed: Color {
-        Color(red: 194 / 255, green: 49 / 255, blue: 44 / 255)
+        WebView(requestURL: URL(string: "https://edenpr.follettdestiny.com/common/welcome.jsp?context=saas087_2222558")!)
+            .edgesIgnoringSafeArea(.all)
+        taskBar(selectedTab: 3)
     }
 }
 
